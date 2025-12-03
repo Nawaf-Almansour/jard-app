57828e8436ed4b39fe7a73665c27817bd5a07b96
nawaf-space <nawaaf.almansour@gmail.com>
2025-12-03 12:46:39 +0300

    update

--- src/data/products.js
+++ src/data/products.js
@@ -18,7 +18,9 @@ export const productsData = [
       { id: 113, name: "كوب مكسرات صغير", unit: "شدة", image: "/inventory/113-nuts-cup-small.jpg" },
       { id: 114, name: "أكياس وزنية الشاي صغير", unit: "شدة", image: "/inventory/114-tea-weight-bags-small.png" },
       { id: 115, name: "أكياس وزنية الشاي كبير", unit: "شدة", image: "/inventory/115-tea-weight-bags-large.png" },
-      { id: 116, name: "ورق كاشير", unit: "كرتون", image: "/inventory/116-cashier-rolls.jpg" }
+      { id: 116, name: "ورق كاشير", unit: "كرتون", image: "/inventory/116-cashier-rolls.jpg" },
+      { id: 117, name: "حليب نادك", unit: "علبة", image: "/inventory/117-nadec-milk.jpg" },
+      { id: 118, name: "حليب ابو قوس", unit: "علبة", image: "/inventory/118-abu-qaws-milk.jpg" }
     ]
   },
   {
@@ -67,17 +69,158 @@ export const productsData = [
       { id: 412, name: "سيروب بنفسجي", unit: "حبة", image: "/inventory/412-syrup-purple.jpg" }
     ]
   },
+  // {
+  //   id: 5,
+  //   category: "❄️ الثلاجة",
+  //   items: [
+  //     { id: 501, name: "جبنة موتزاريلا", unit: "كيس", image: "/inventory/501-mozzarella-cheese.jpg" },
+  //     { id: 502, name: "كريمة طبخ", unit: "علبة", image: "/inventory/502-cooking-cream.jpg" },
+  //     { id: 503, name: "طماطم معلبة", unit: "علبة", image: "/inventory/503-canned-tomatoes.jpg" },
+  //     { id: 504, name: "ملح", unit: "علبة", image: "/inventory/504-salt.jpg" },
+  //     { id: 505, name: "زيت قلي", unit: "لتر", image: "/inventory/505-frying-oil.jpg" },
+  //     { id: 506, name: "زيت زيتون", unit: "لتر", image: "/inventory/506-olive-oil.jpg" },
+  //     { id: 507, name: "بهار فلفل أسود", unit: "علبة", image: "/inventory/507-black-pepper.jpg" }
+  //   ]
+  // },
   {
-    id: 5,
-    category: "❄️ الثلاجة",
+    id: 6,
+    category: "🧀 الأجبان ومنتجات الألبان",
     items: [
-      { id: 501, name: "جبنة موتزاريلا", unit: "كيس", image: "/inventory/501-mozzarella-cheese.jpg" },
-      { id: 502, name: "كريمة طبخ", unit: "علبة", image: "/inventory/502-cooking-cream.jpg" },
-      { id: 503, name: "طماطم معلبة", unit: "علبة", image: "/inventory/503-canned-tomatoes.jpg" },
-      { id: 504, name: "ملح", unit: "علبة", image: "/inventory/504-salt.jpg" },
-      { id: 505, name: "زيت قلي", unit: "لتر", image: "/inventory/505-frying-oil.jpg" },
-      { id: 506, name: "زيت زيتون", unit: "لتر", image: "/inventory/506-olive-oil.jpg" },
-      { id: 507, name: "بهار فلفل أسود", unit: "علبة", image: "/inventory/507-black-pepper.jpg" }
+      { id: 601, name: "جبن موزاريلا", unit: "كيس", image: "/inventory/601-mozzarella.jpg" },
+      { id: 602, name: "جبنه شيدر", unit: "بوكس", image: "/inventory/602-cheddar.jpg" },
+      { id: 603, name: "جبنه قشقوان", unit: "بوكس", image: "/inventory/603-kashkaval.jpg" },
+      { id: 604, name: "جبنه عكاوي", unit: "بوكس", image: "/inventory/604-akawi.jpg" },
+      { id: 605, name: "جبنة برميزان", unit: "بوكس", image: "/inventory/605-parmesan.jpg" },
+      { id: 606, name: "جبنه كيري سايل", unit: "علبه", image: "/inventory/6013-kiri-s.jpg" },
+      { id: 6013, name: " جبنه كيري", unit: "علبه", image: "/inventory/606-kiri.jpg" },
+      { id: 607, name: "جبنه كرافت فلادلفيا", unit: "علبه", image: "/inventory/607-philadelphia.jpg" },
+      { id: 608, name: "كريمه طبخ", unit: "علبه", image: "/inventory/608-cooking-cream.jpg" },
+      { id: 609, name: "كريمه خفق", unit: "علبه", image: "/inventory/609-whipping-cream.jpg" },
+      { id: 610, name: "زبادي نادك", unit: "شده", image: "/inventory/610-nadec-yogurt.jpg" },
+      { id: 611, name: "قشطه المراعي", unit: "علبه", image: "/inventory/611-almarai-cream.png" },
+      { id: 612, name: "زبده نادك", unit: "علبه", image: "/inventory/612-nadec-butter.jpg" }
+    ]
+  },
+  {
+    id: 7,
+    category: "🍗 اللحوم والبروتين",
+    items: [
+      { id: 701, name: "صدور دجاج", unit: "كيس", image: "/inventory/701-chicken-breast.jpg" },
+      { id: 702, name: "ببروني", unit: "بوكس", image: "/inventory/702-pepperoni.jpg" },
+      { id: 703, name: " فول مقشر", unit: "كيس", image: "/inventory/703-fava-beans.jpg" },
+      { id: 705, name: "حب كامل  فول", unit: "كيس", image: "/inventory/705-fava-beans.jpg" },
+      { id: 704, name: "حمص", unit: "كيس", image: "/inventory/704-chickpeas.jpg" }
+    ]
+  },
+  {
+    id: 8,
+    category: "🥦 الخضار والفواكه الطازجة",
+    items: [
+      { id: 801, name: "طماط", unit: "صحن", image: "/inventory/801-tomatoes.jpg" },
+      { id: 802, name: "فلفل بارد", unit: "حبه", image: "/inventory/802-bell-pepper.jpg" },
+      { id: 803, name: "فلفل حار", unit: "حبه", image: "/inventory/803-hot-pepper.jpg" },
+      { id: 804, name: "فطر", unit: "بوكس", image: "/inventory/804-mushroom.jpg" },
+      { id: 805, name: "بصل", unit: "كيس", image: "/inventory/805-onions.jpg" },
+      { id: 806, name: "بطاطس", unit: "كيس", image: "/inventory/806-potatoes.jpg" },
+      { id: 807, name: "خس", unit: "ربطه", image: "/inventory/807-lettuce.jpg" },
+      { id: 808, name: "كزبره", unit: "ربطه", image: "/inventory/808-coriander.jpg" },
+      { id: 809, name: "بقدونس", unit: "ربطه", image: "/inventory/809-parsley.jpg" },
+      { id: 810, name: "شبث", unit: "ربطه", image: "/inventory/810-dill.jpg" },
+      { id: 811, name: "ليمون", unit: "صحن", image: "/inventory/811-lemon.jpg" },
+      { id: 812, name: "ريحان", unit: "بوكس", image: "/inventory/812-basil.jpg" }
+    ]
+  },
+  {
+    id: 9,
+    category: "🫒 المخللات والزيتون",
+    items: [
+      { id: 901, name: "زيتون مشكل مشوي", unit: "بوكس", image: "/inventory/901-grilled-olives.jpg" },
+      { id: 902, name: "زيتون كلمنتان", unit: "بوكس", image: "/inventory/902-kalamata-olives.jpg" },
+      { id: 903, name: "مخلل لفت", unit: "بوكس", image: "/inventory/903-pickled-turnip.jpg" }
+    ]
+  },
+  {
+    id: 10,
+    category: "🥫 المواد المعلبة والمجففة",
+    items: [
+      { id: 1001, name: "طماطم حب مقشره", unit: "علبه", image: "/inventory/1001-peeled-tomatoes.jpg" },
+      { id: 1002, name: "صلصلة الطماطم", unit: "علبه", image: "/inventory/1002-tomato-sauce.jpg" },
+      { id: 1003, name: "حلاوه شعر طحينيه", unit: "بوكس", image: "/inventory/1003-halawa.jpg" }
+    ]
+  },
+  {
+    id: 11,
+    category: "🌿 الأعشاب والبهارات",
+    items: [
+      { id: 1101, name: "زعتر مطحون", unit: "كيس", image: "/inventory/1101-ground-zaatar.jpg" },
+      { id: 1102, name: "سماق", unit: "كيس", image: "/inventory/1102-sumac.jpg" },
+      { id: 1103, name: "بهارات", unit: "علبه", image: "/inventory/1103-spices.jpg" },
+      { id: 1104, name: "صنوبر", unit: "كيس", image: "/inventory/1104-pine-nuts.jpg" },
+      { id: 1105, name: "سمس حبه سودا", unit: "كيس", image: "/inventory/1105-black-sesame.jpg" },
+      { id: 1106, name: "زعتر اوريجانو", unit: "علبه", image: "/inventory/1106-oregano.jpg" },
+      { id: 1107, name: "فلفل اسود مطحون", unit: "علبه", image: "/inventory/1107-black-pepper.jpg" },
+      { id: 1108, name: "فلفل ابيض مطحون", unit: "علبه", image: "/inventory/1108-white-pepper.jpg" },
+      { id: 1109, name: "كزبره مطحونه", unit: "علبه", image: "/inventory/1109-ground-coriander.jpg" },
+      { id: 1110, name: "كمون مطحون", unit: "علبه", image: "/inventory/1110-ground-cumin.jpg" },
+      { id: 1111, name: "بهارات مشكله ناعمه مهباج", unit: "علبه", image: "/inventory/1111-mixed-spices.jpg" },
+      { id: 1112, name: "بابريكا فلفل حار", unit: "علبه", image: "/inventory/1112-paprika.jpg" },
+      { id: 1113, name: "ملح", unit: "علبه", image: "/inventory/1113-salt.jpg" },
+      { id: 1114, name: "ملح الليمون", unit: "علبه", image: "/inventory/1114-citric-acid.jpg" }
+    ]
+  },
+  {
+    id: 12,
+    category: "🍞 المخبوزات والمواد الجافة",
+    items: [
+      { id: 1201, name: "دقيق بُر", unit: "كيس", image: "/inventory/1201-whole-wheat-flour.jpg" },
+      { id: 1202, name: "دقيق الأولى", unit: "كيس", image: "/inventory/1202-first-flour.jpg" },
+      { id: 1203, name: "شابوره تراميسو", unit: "علبه", image: "/inventory/1203-tiramisu-biscuits.jpg" },
+      { id: 1204, name: "دقيق بيتزا", unit: "كيس", image: "/inventory/1204-pizza-flour.jpg" }
+    ]
+  },
+  {
+    id: 13,
+    category: "🍰 الحلويات ومستلزماتها",
+    items: [
+      { id: 1301, name: "كراميل", unit: "بكت", image: "/inventory/1301-caramel.jpg" },
+      { id: 1302, name: "فانيلا", unit: "علبه", image: "/inventory/1302-vanilla.jpg" },
+      { id: 1303, name: "باكينج باودر", unit: "علبه", image: "/inventory/1303-baking-powder.jpg" },
+      { id: 1304, name: "بيكربونات الصوديوم", unit: "علبه", image: "/inventory/1304-baking-soda.jpg" },
+      { id: 1305, name: "عسل", unit: "علبه", image: "/inventory/1305-honey.jpg" }
+    ]
+  },
+  {
+    id: 14,
+    category: "🧊 المجمدات",
+    items: [
+      { id: 1401, name: "مانجو مجمد", unit: "كيسه", image: "/inventory/1401-frozen-mango.jpg" }
+    ]
+  },
+  {
+    id: 15,
+    category: "🍗 إضافات الطهي الأساسية",
+    items: [
+      { id: 1501, name: "ماجي", unit: "كرتون", image: "/inventory/1501-maggi.jpg" },
+      { id: 1502, name: "خميره", unit: "بكت", image: "/inventory/1502-yeast.jpg" },
+      { id: 1503, name: "محسن خبز إنرجي فايف", unit: "علبه", image: "/inventory/1503-bread-improver.jpg" }
+    ]
+  },
+  {
+    id: 16,
+    category: "🧂 الصوصات",
+    items: [
+      { id: 1601, name: "كاتشب", unit: "علبه", image: "/inventory/1601-ketchup.jpg" },
+      { id: 1602, name: "شطه رنا", unit: "علبه", image: "/inventory/1602-rana-hot-sauce.jpg" },
+      { id: 1603, name: "باربكيو صوص", unit: "علبه", image: "/inventory/1603-bbq-sauce.jpg" },
+      { id: 1604, name: "طحينيه صوص", unit: "علبه", image: "/inventory/1604-tahini-sauce.jpg" },
+      { id: 1605, name: "خردل", unit: "علبه", image: "/inventory/1605-mustard.jpg" }
+    ]
+  },
+  {
+    id: 17,
+    category: "🥔 السناكس",
+    items: [
+      { id: 1701, name: "بطاطس عمان", unit: "كيس", image: "/inventory/1701-oman-chips.jpg" }
     ]
   }
 ];
